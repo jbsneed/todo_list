b9526e57653acc6e92699f0d4284a8a8ffdc8565
jbsneed <52945300+jbsneed@users.noreply.github.com>
2019-12-22 22:02:53 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,88 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="container">
+    <h1>ToDo List</h1>
+    <md-field>
+      <md-input
+        v-model="currentTodo"
+        @keydown.enter="addTodo()"
+        placeholder="Click here to add an item to your ToDo List"
+      ></md-input>
+    </md-field>
+    <ul class="todos">
+      <li v-for="todo in todos" :key="todo.id">
+        <input class="completeButton" type="checkbox" v-model="todo.completed" />
+        <span
+          class="todo-item-label"
+          :class="{completed: todo.completed}"
+          @dblclick="editTodo(todo)"
+          v-if="!todo.edit"
+        >{{todo.label}}</span>
+        <md-input
+          v-else
+          class="todo-item-edit"
+          type="text"
+          v-model="todo.label"
+          @keyup.enter="completedEdit(todo)"
+        ></md-input>
+        <md-raised class="closeButton" @click="removeTodo(todo)">Delete</md-raised>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      currentTodo: "",
+      editedTodo: null
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        edit: false
+      });
+      this.currentTodo = "";
+    },
+    editTodo(todo) {
+      todo.edit = true;
+    },
+    completedEdit(todo) {
+      todo.edit = false;
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
     }
   }
+};
+</script>
+
+<style>
+.container {
+  background-color: #1f2833;
+  color: #66fcf1;
+  text-align: center;
+}
+li {
+  list-style: none;
+}
+.completed {
+  text-decoration: line-through;
+}
+.closeButton {
+  float: right;
+}
+.closeButton:hover {
+  cursor: pointer;
+  color: red;
+}
+.todo-item-label:hover {
+  cursor: grab;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import App from './App.vue';
+import { MdField } from 'vue-material/dist/components';
+import 'vue-material/dist/vue-material.min.css';
 import './registerServiceWorker';
-import router from './router';
-import store from './store';
+
+Vue.use(MdField);
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+  render: h => h(App),
+}).$mount('#app')
